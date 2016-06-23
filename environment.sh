37936efa73aa977c5e7cd56274e23e20aefca261
Bill Cawthra <bill.cawthra@pokitdok.com>
2016-06-22 21:16:56 -0400

    added virtualenv logic

--- environment.sh
+++ environment.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-virtualenv env
-source ./env/bin/activate
-pip install -U boto boto3 ansible
-git clone https://github.com/ansible/ansible-container.git
+if [ ! -d ./env/bin/activate ]; then
+  virtualenv env
+  source ./env/bin/activate
+  pip install -U boto boto3 ansible
+else:
+  source ./env/bin/activate
+fi
+
+if [ ! -d ansible-container ]; then
+  git clone https://github.com/ansible/ansible-container.git
+fi
